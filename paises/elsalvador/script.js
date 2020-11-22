1f331aa828b7609ba03a39f41825680a104c7173
Rodolfo Vargas <rodolfovargas.files@gmail.com>
2020-11-22 17:29:19 -0600

    Logré hacer un menú despegables de las rutas del país

--- paises/elsalvador/script.js
+++ paises/elsalvador/script.js
@@ -9,10 +9,10 @@ var ruta1Botton2 = document.getElementById('ruta1-botton-2');
 var ruta1Botton3 = document.getElementById('ruta1-botton-3');
 var ruta1Botton4 = document.getElementById('ruta1-botton-4')
 //botonesSliderRuta1
-ruta1Botton1.addEventListener("click", slide1)
-ruta1Botton2.addEventListener("click", slide2)
-ruta1Botton3.addEventListener("click", slide3)
-ruta1Botton4.addEventListener("click", slide4)
+ruta1Botton1.addEventListener("click", ruta1slide1)
+ruta1Botton2.addEventListener("click", ruta1slide2)
+ruta1Botton3.addEventListener("click", ruta1slide3)
+ruta1Botton4.addEventListener("click", ruta1slide4)
 
 //botonesRutasTitle
 var ruta1BottonTitle = document.getElementById('mainRuta1');
@@ -21,8 +21,9 @@ var contador =  0;
 
 ruta1BottonTitle.addEventListener("click", desplegarRuta1)
 
-function desplegarRuta1()
+function desplegarRuta1(evento)
 {  
+    console.log(evento);
     if(contador==0)
     {
         ruta1Content.style.animationPlayState = "running";
@@ -36,7 +37,7 @@ function desplegarRuta1()
     }
 }
 
-function slide1()
+function ruta1slide1()
 {
     ruta1Botton1.style.backgroundColor = "black";
     ruta1Botton2.style.backgroundColor = "rgb(119, 119, 119)";
@@ -47,7 +48,7 @@ function slide1()
     ruta1Slider3.style.opacity = "0";
     ruta1Slider4.style.opacity = "0";
 }
-function slide2()
+function ruta1slide2()
 {
     ruta1Botton2.style.backgroundColor = "black";
     ruta1Botton1.style.backgroundColor = "rgb(119, 119, 119)";
@@ -58,7 +59,7 @@ function slide2()
     ruta1Slider3.style.opacity = "0";
     ruta1Slider4.style.opacity = "0";
 }
-function slide3()
+function ruta1slide3()
 {
     ruta1Botton3.style.backgroundColor = "black";
     ruta1Botton1.style.backgroundColor = "rgb(119, 119, 119)";
@@ -69,7 +70,7 @@ function slide3()
     ruta1Slider2.style.opacity = "0";
     ruta1Slider4.style.opacity = "0";
 }
-function slide4()
+function ruta1slide4()
 {
     ruta1Botton4.style.backgroundColor = "black";
     ruta1Botton1.style.backgroundColor = "rgb(119, 119, 119)";
@@ -79,4 +80,89 @@ function slide4()
     ruta1Slider1.style.opacity = "0";
     ruta1Slider2.style.opacity = "0";
     ruta1Slider3.style.opacity = "0";
+}
+
+
+//SlidersRuta2
+var ruta2Slider1 = document.getElementById('ruta2-slider-1');
+var ruta2Slider2 = document.getElementById('ruta2-slider-2');
+var ruta2Slider3 = document.getElementById('ruta2-slider-3');
+var ruta2Slider4 = document.getElementById('ruta2-slider-4');
+
+var ruta2Botton1 = document.getElementById('ruta2-botton-1');
+var ruta2Botton2 = document.getElementById('ruta2-botton-2');
+var ruta2Botton3 = document.getElementById('ruta2-botton-3');
+var ruta2Botton4 = document.getElementById('ruta2-botton-4')
+//botonesSliderRuta2
+ruta2Botton1.addEventListener("click", ruta2slide1)
+ruta2Botton2.addEventListener("click", ruta2slide2)
+ruta2Botton3.addEventListener("click", ruta2slide3)
+ruta2Botton4.addEventListener("click", ruta2slide4)
+
+//botonesRutasTitle2
+var ruta2BottonTitle = document.getElementById('mainRuta2');
+var ruta2Content = document.getElementById('contentRuta2');
+var contador2 =  0;
+
+ruta2BottonTitle.addEventListener("click", desplegarRuta2)
+
+function desplegarRuta2(evento)
+{  
+    console.log(evento);
+    if(contador2==0)
+    {
+        ruta2Content.style.animationPlayState = "running";
+        ruta2Content.style.display = "grid";
+        contador2 = 1;
+    }
+    else if (contador2==1)
+    {
+        ruta2Content.style.display = "none";
+        contador2 = 0;
+    }
+}
+
+function ruta2slide1()
+{
+    ruta2Botton1.style.backgroundColor = "black";
+    ruta2Botton2.style.backgroundColor = "rgb(119, 119, 119)";
+    ruta2Botton3.style.backgroundColor = "rgb(119, 119, 119)";
+    ruta2Botton4.style.backgroundColor = "rgb(119, 119, 119)";
+    ruta2Slider1.style.opacity = "1";
+    ruta2Slider2.style.opacity = "0";
+    ruta2Slider3.style.opacity = "0";
+    ruta2Slider4.style.opacity = "0";
+}
+function ruta2slide2()
+{
+    ruta2Botton2.style.backgroundColor = "black";
+    ruta2Botton1.style.backgroundColor = "rgb(119, 119, 119)";
+    ruta2Botton3.style.backgroundColor = "rgb(119, 119, 119)";
+    ruta2Botton4.style.backgroundColor = "rgb(119, 119, 119)";
+    ruta2Slider2.style.opacity = "1";
+    ruta2Slider1.style.opacity = "0";
+    ruta2Slider3.style.opacity = "0";
+    ruta2Slider4.style.opacity = "0";
+}
+function ruta2slide3()
+{
+    ruta2Botton3.style.backgroundColor = "black";
+    ruta2Botton1.style.backgroundColor = "rgb(119, 119, 119)";
+    ruta2Botton2.style.backgroundColor = "rgb(119, 119, 119)";
+    ruta2Botton4.style.backgroundColor = "rgb(119, 119, 119)";
+    ruta2Slider3.style.opacity = "1";
+    ruta2Slider1.style.opacity = "0";
+    ruta2Slider2.style.opacity = "0";
+    ruta2Slider4.style.opacity = "0";
+}
+function ruta2slide4()
+{
+    ruta2Botton4.style.backgroundColor = "black";
+    ruta2Botton1.style.backgroundColor = "rgb(119, 119, 119)";
+    ruta2Botton2.style.backgroundColor = "rgb(119, 119, 119)";
+    ruta2Botton3.style.backgroundColor = "rgb(119, 119, 119)";
+    ruta2Slider4.style.opacity = "1";
+    ruta2Slider1.style.opacity = "0";
+    ruta2Slider2.style.opacity = "0";
+    ruta2Slider3.style.opacity = "0";
 }
